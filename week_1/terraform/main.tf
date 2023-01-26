aab664194b3a1323cc08cffc85fa1cfdf87c83cf
aRuN0012 <15euee012@skcet.ac.in>
2023-01-26 15:44:08 +0530

    terraform changes

---
+++ week_1/terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">=1.3"
+  backend "local" {}
+  required_providers {
+    google={
+      source="hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  region = var.region
+  project = var.project
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name = "${local.data_lake_bucket}_${var.project}"
+  location      = var.region
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "dataset" {
+    dataset_id = var.BQ_DATASET
+    project    = var.project
+    location   = var.region
+}
